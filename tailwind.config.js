d96f2045b7758084f0f1201b5c28c6d37989d44a
akhilsuruj5 <akhilsuruj5@gmail.com>
2024-12-09 01:08:01 +0530

    deploy err

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.js (ES Module Syntax)
+export default {
   content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
